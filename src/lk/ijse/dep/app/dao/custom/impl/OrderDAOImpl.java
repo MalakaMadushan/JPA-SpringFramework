876a28f37c2f4f0af62c9573b6490adbfca35c72
malakamadushan <malakamadushan123@gmail.com>
2019-02-20 22:02:01 +0530

    first implementation

---
+++ src/lk/ijse/dep/app/dao/custom/impl/OrderDAOImpl.java
@@ -0,0 +1,17 @@
+package lk.ijse.dep.app.dao.custom.impl;
+
+import lk.ijse.dep.app.dao.CrudDAOImpl;
+import lk.ijse.dep.app.dao.custom.OrderDAO;
+import lk.ijse.dep.app.entity.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderDAOImpl extends CrudDAOImpl<Order,String> implements OrderDAO {
+
+    @Override
+    public Long count() throws Exception {
+        Long count= (Long) em.createQuery("SELECT COUNT(o) FROM Order o").getSingleResult();
+        System.out.println(count);
+        return count;
+    }
+}
